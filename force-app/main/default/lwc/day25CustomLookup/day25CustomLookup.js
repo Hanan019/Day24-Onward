cdb09feb71fa605021d32d9722b37096b64cd260
Hanan Maqbol <hananmaqbol1@outlook.com>
2024-03-12 10:07:06 +0500

    Created a class and a lightning component for a Custom Lookup field creation

--- force-app/main/default/lwc/day25CustomLookup/day25CustomLookup.js
+++ force-app/main/default/lwc/day25CustomLookup/day25CustomLookup.js
@@ -1,25 +1,60 @@
 import { LightningElement, wire } from "lwc";
 import searchRecords from "@salesforce/apex/customLookupController.searchRecords";
 
+const DELAY = 300;
 export default class Day25CustomLookup extends LightningElement {
-  apiName = "Accuont";
-  searchValue = "A";
+  apiName = "Account";
+  searchValue;
   objectLabel = "Account";
   iconName = "standard:account";
-
+  delayTimeout;
+  selectedRecord = {
+    selectedId: "",
+    selectedName: ""
+  };
+  displayOptions = false;
   @wire(searchRecords, {
     objectApiName: "$apiName",
     searchKey: "$searchValue"
   })
-  outputsFunction({ data, error }) {
-    if (data) {
-      console.log("data  : ", data);
-    } else if (error) {
-      console.log("error....... : ", error);
-    }
+  outputs;
+  get isRecordSelected() {
+    return this.selectedRecord.selectedId === "" ? false : true;
   }
 
-  //   get showOutput() {
-  //     return this.outputs.data;
-  //   }
+  get showOutput() {
+    return this.outputs.data;
+  }
+  changeHandler(event) {
+    window.clearTimeout(this.delayTimeout);
+    let enteredValue = event.target.value;
+
+    // Debouncing: execute search after a delay
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    this.delayTimeout = setTimeout(() => {
+      this.searchValue = enteredValue;
+      this.displayOptions = true;
+    }, DELAY);
+  }
+
+  clickHandler(event) {
+    let selectedId = event.currentTarget.dataset.item;
+    console.log("selectedId ", selectedId);
+    let outputRecord = this.outputs.data.find(
+      (currItem) => currItem.Id === selectedId
+    );
+    this.selectedRecord = {
+      selectedId: outputRecord.Id,
+      selectedName: outputRecord.Name
+    };
+    this.displayOptions = false;
+  }
+
+  removalSelectionHandler(event) {
+    this.selectedRecord = {
+      selectedId: "",
+      selectedName: ""
+    };
+    this.displayOptions = false;
+  }
 }
