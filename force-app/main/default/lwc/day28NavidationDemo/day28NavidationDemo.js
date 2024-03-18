7bfdda1d9b88d31fcec92df2b9b47ecf00c99573
Hanan Maqbol <hananmaqbol1@outlook.com>
2024-03-18 11:00:06 +0500

    Have added two more buttons in the lightning compoent which will navigate to te account list and on on the create account page.

--- force-app/main/default/lwc/day28NavidationDemo/day28NavidationDemo.js
+++ force-app/main/default/lwc/day28NavidationDemo/day28NavidationDemo.js
@@ -1,5 +1,5 @@
 import { LightningElement } from "lwc";
-import { NavigationMixin } from "Lightning/navigation";
+import { NavigationMixin } from "lightning/navigation";
 export default class Day28NavidationDemo extends NavigationMixin(
   LightningElement
 ) {
@@ -10,6 +10,35 @@ export default class Day28NavidationDemo extends NavigationMixin(
         pageName: "home"
       }
     };
+    this[NavigationMixin.Navigate](pageref);
+  }
+
+  accListHandler() {
+    let pageRef =
+      // Navigates to account list with the filter set to RecentlyViewedAccounts.
+      {
+        type: "standard__objectPage",
+        attributes: {
+          objectApiName: "Account",
+          actionName: "list"
+        },
+        state: {
+          filterName: "PlatinumandGoldSLACustomers"
+        }
+      };
+    this[NavigationMixin.Navigate](pageRef);
+  }
+  CreateAccountHandler() {
+    let pageref =
+      //this function will be used when
+      //we dont want to populate the default values.
+      {
+        type: "standard__objectPage",
+        attributes: {
+          objectApiName: "Account",
+          actionName: "new"
+        }
+      };
     this[NavigationMixin.Navigate](pageref);
   }
 }
