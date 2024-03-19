7f03388c70f18a537760742197d597268098e008
Hanan Maqbol <hananmaqbol1@outlook.com>
2024-03-19 10:21:29 +0500

    Corrected the code for the component.

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,13 +1,14 @@
 import { LightningElement, wire } from "lwc";
 import getContactList from "@salesforce/apex/contactController.getContactList";
+
 export default class ContactList extends LightningElement {
   @wire(getContactList) contacts;
+  selectedContact;
 
   selectionHandler(event) {
-    let selectedContactId = event.detail;
-
-    this.selectedContactId = this.contacts.data.find(
-      (currItem) => currItem.Id === selectedContactId
+    const selectedContactId = event.detail;
+    this.selectedContact = this.contacts.data.find(
+      (contact) => contact.Id === selectedContactId
     );
   }
 }
